4585de251029a69da9b41a002c94693091a196ba
laquizhpe <laquizhpe@alu.ucam.edu>
2025-06-20 09:56:31 +0200

    Mejora: Encapsulados los atributos de Alumno y añadidos getters/setters con validación de notas entre 0 y 10

--- Alumno.java
+++ Alumno.java
@@ -1,14 +1,58 @@
 public class Alumno {
-    public String nombre;
-    public double nota1;
-    public double nota2;
-    public double nota3;
+    private String nombre;
+    private double nota1;
+    private double nota2;
+    private double nota3;
 
     public Alumno(String n, double n1, double n2, double n3) {
         nombre = n;
-        nota1 = n1;
-        nota2 = n2;
-        nota3 = n3;
+        setNota1(n1);
+        setNota2(n2);
+        setNota3(n3);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getNota1() {
+        return nota1;
+    }
+
+    public void setNota1(double nota1) {
+        if (nota1 >= 0 && nota1 <= 10) {
+            this.nota1 = nota1;
+        } else {
+            throw new IllegalArgumentException("La nota1 debe estar entre 0 y 10");
+        }
+    }
+
+    public double getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(double nota2) {
+        if (nota2 >= 0 && nota2 <= 10) {
+            this.nota2 = nota2;
+        } else {
+            throw new IllegalArgumentException("La nota2 debe estar entre 0 y 10");
+        }
+    }
+
+    public double getNota3() {
+        return nota3;
+    }
+
+    public void setNota3(double nota3) {
+        if (nota3 >= 0 && nota3 <= 10) {
+            this.nota3 = nota3;
+        } else {
+            throw new IllegalArgumentException("La nota3 debe estar entre 0 y 10");
+        }
     }
 
     public double promedio() {
